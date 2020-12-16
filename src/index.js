f8dd849f9c1977596b02f753f1bdf02501c00996
RomanKonopelko <romank761@gmail.com>
2020-12-16 13:12:41 +0200

    colorize-done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
 import "./styles.css";
 import { colors } from "./js/colors.js";
 import refs from "./js/refs.js";
+
 const { startBtn, h1, body, pauseBtn } = refs;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+let intervalId = null;
+let startColorize = null;
+let pauseColorize = null;
 
 const colorizeItems = (time) => {
-  setInterval(() => {
+  startColorize = setInterval(() => {
     startBtn.style.backgroundColor =
       "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }, time);
+  pauseColorize = setInterval(() => {
     pauseBtn.style.backgroundColor =
       "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }, time);
+
+  setInterval(() => {
     h1.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }, time);
 };
 
 colorizeItems(200);
 
-const bodyColorize = (time) => {
-  const bodyColor = setInterval(() => {
+const bodyColorize = () => {
+  // if (intervalId) {
+  //   return;
+  // }
+  intervalId = setInterval(() => {
+    startBtn.disabled = true;
     body.style.backgroundColor =
       colors[randomIntegerFromInterval(0, colors.length - 1)];
-  }, time);
+  }, 800);
 };
 
-startBtn.addEventListener("click", () => bodyColorize(100));
-pauseBtn.addEventListener("click", () => clearInterval());
+const bodyClear = () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  body.style.backgroundColor = "";
+  intervalId = null;
+};
+
+startBtn.addEventListener("click", bodyColorize);
+
+pauseBtn.addEventListener("click", bodyClear);
